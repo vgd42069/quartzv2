42b3945489cc0d3ac49b2c6d0b9b1286622c712a
vgd42069 <vdepalma09@gmail.com>
2026-02-18 19:45:42 -0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '40px', fontFamily: 'sans-serif' }}>
+      <h1 style={{ color: '#9333ea' }}>🎉 Quartz Markets</h1>
+      <p>Your app is successfully deployed!</p>
+    </div>
+  );
+}
+
+export default App;
